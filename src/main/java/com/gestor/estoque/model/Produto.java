4795fa40d4c144362a99a8ed1915bb228b260e7a
Bruder <ibruder83@gmail.com>
2026-09-11 00:28:15 -0300

    Update Produto.java

--- src/main/java/com/gestor/estoque/model/Produto.java
+++ src/main/java/com/gestor/estoque/model/Produto.java
@@ -1,12 +1,16 @@
 package com.gestor.estoque.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "produtos")
@@ -23,35 +27,18 @@ public class Produto {
     @JoinColumn(name = "usuario_id")
     private Usuario usuario;
 
-    public Long getId() {
-        return id;
-    }
+    // A MÁGICA ACONTECE AQUI
+    @OneToMany(mappedBy = "produto", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ItemReceita> receita = new ArrayList<>();
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(Double preco) {
-        this.preco = preco;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public Double getPreco() { return preco; }
+    public void setPreco(Double preco) { this.preco = preco; }
+    public Usuario getUsuario() { return usuario; }
+    public void setUsuario(Usuario usuario) { this.usuario = usuario; }
+    public List<ItemReceita> getReceita() { return receita; }
+    public void setReceita(List<ItemReceita> receita) { this.receita = receita; }
 }
