f68713fea5cf0cf7c6d8fa2dda2695fb7d0b2894
Bruder <ibruder83@gmail.com>
2026-09-11 00:34:57 -0300

    Remove Lombok and add manual getters/setters

--- src/main/java/com/gestor/estoque/dto/ItemReceitaDTO.java
+++ src/main/java/com/gestor/estoque/dto/ItemReceitaDTO.java
@@ -1,9 +1,15 @@
 package com.gestor.estoque.dto;
 
-import lombok.Data;
-
-@Data
 public class ItemReceitaDTO {
+    
     private Long ingredienteId;
     private Double quantidadeNecessaria;
+
+    public ItemReceitaDTO() {}
+
+    public Long getIngredienteId() { return ingredienteId; }
+    public void setIngredienteId(Long ingredienteId) { this.ingredienteId = ingredienteId; }
+    
+    public Double getQuantidadeNecessaria() { return quantidadeNecessaria; }
+    public void setQuantidadeNecessaria(Double quantidadeNecessaria) { this.quantidadeNecessaria = quantidadeNecessaria; }
 }
